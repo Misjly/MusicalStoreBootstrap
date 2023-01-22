8367fdc6df18e8532964c3c7ed50cc85caf0ed34
Misjly <nikmisg@gmail.com>
2023-01-22 14:13:47 -0600

    hover/focus/activation fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,14 @@ $(function() {
     $("#findButton").on("click", function() {
         $("#findModal").modal();
     })
+
+    $("#labelYes").on("click", function(){
+        $(this).addClass("activated");
+        $("#labelNo").removeClass("activated");
+     });
+
+     $("#labelNo").on("click", function(){
+        $(this).addClass("activated");
+        $("#labelYes").removeClass("activated");
+     });
 })
