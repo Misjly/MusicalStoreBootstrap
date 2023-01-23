54d1d7dfd9bad4a98056443fbbe6f198bb5ae337
Misjly <nikmisg@gmail.com>
2023-01-22 21:59:29 -0600

    multiple-item carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,44 @@
-$(function() {
-    $("#findButton").on("click", function() {
-        $("#findModal").modal();
-    })
+$(function () {
+  $("#findButton").on("click", function () {
+    $("#findModal").modal();
+  });
 
-    $("#labelYes").on("click", function(){
-        $(this).addClass("activated");
-        $("#labelNo").removeClass("activated");
-     });
+  $("#labelYes").on("click", function () {
+    $(this).addClass("activated");
+    $("#labelNo").removeClass("activated");
+  });
 
-     $("#labelNo").on("click", function(){
-        $(this).addClass("activated");
-        $("#labelYes").removeClass("activated");
-     });
-})
+  $("#labelNo").on("click", function () {
+    $(this).addClass("activated");
+    $("#labelYes").removeClass("activated");
+  });
+});
+
+var carouselWidth;
+var cardWidth;
+var scrollPosition = 0;
+let numberOfCards = 3;
+
+$(".carousel-control-next").on("click", function () {
+  cardWidth = $(".carousel-item").width();
+  carouselWidth = $(".carousel-inner")[0].scrollWidth;
+
+  if (scrollPosition < carouselWidth - cardWidth * numberOfCards) {
+    scrollPosition += cardWidth;
+  }
+  else {
+    scrollPosition = 0;
+  }
+  $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 1000);
+});
+
+$(".carousel-control-prev").on("click", function () {
+    cardWidth = $(".carousel-item").width();
+    if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+    }
+    else {
+        scrollPosition = carouselWidth - cardWidth * numberOfCards;
+    }
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 1000);
+});
